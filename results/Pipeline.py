519fe40efd2585801b88fd773082fad987ca550f
James Bornholt <u4842199@anu.edu.au>
2011-07-04 11:10:13 +1000

    delete old cruft

--- results/Pipeline.py
+++ results/Pipeline.py
@@ -114,37 +114,3 @@ class Pipeline(object):
                 graph_outputs.append(ret)
 
         return graph_outputs
-
-
-def execute_pipeline(encoded_string, csv_graphs=False):
-    decoded = plotty.results.PipelineEncoder.decode_pipeline(encoded_string)
-    try:
-        dt = DataTable(logs=decoded['logs'])
-        dt.selectValueColumns(decoded['value_columns'])
-        dt.selectScenarioColumns(decoded['scenario_columns'])
-        logging.debug('Initial: %s' % dt.scenarioColumns)
-    except:
-        raise PipelineLoadException(*sys.exc_info())
-    
-    graph_outputs = []
-    for i, block in enumerate(decoded['blocks']):
-        old_dt = copy.deepcopy(dt)
-        try:
-            if block['type'] == 'aggregate':
-                AggregateBlock().process(dt, **block['params'])
-            elif block['type'] == 'filter':
-                FilterBlock().process(dt, block['filters'])
-            elif block['type'] == 'normalise':
-                NormaliseBlock().process(dt, **block['params'])
-            elif block['type'] == 'graph':
-                graph_outputs.append(GraphBlock().process(dt, pipeline_hash=encoded_string, renderCSV=csv_graphs, **block['params']))
-            logging.debug('After block %d (%s): %s' % (i, block['type'], dt.scenarioColumns))
-        except PipelineAmbiguityException as e:
-            e.block = i
-            e.datatable = old_dt
-            e.graph_outputs = graph_outputs
-            raise e
-        except:
-            raise PipelineBlockException(i, *sys.exc_info())
-    
-    return dt, graph_outputs
