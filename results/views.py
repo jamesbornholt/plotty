0f76da3d32cd8e57929408e38ef290b6359c756b
James Bornholt <u4842199@anu.edu.au>
2012-03-27 12:43:11 +1100

    "fix" debug output when graphs are involved

--- results/views.py
+++ results/views.py
@@ -52,11 +52,9 @@ def list(request, pipeline):
     
     output = ''
     if len(graph_outputs) > 0:
-        for i, graph_set in enumerate(graph_outputs, start=1):
-            titles = graph_set.keys()
-            titles.sort()
-            for title in titles:
-                output += '<div class="foldable"><h1>' + title + ' (block ' + str(i) + ')</h1>' + graph_set[title] + '</div>'
+        #for i, graph_set in enumerate(graph_outputs, start=1):
+        #    for graph in graph_set:
+        #        output += '<div class="foldable"><h1>' + graph['title'] + ' (block ' + str(i) + ')</h1>' + graph['hash'] + '</div>'
         output += '<div class="foldable"><h1>Table</h1>' + p.dataTable.renderToTable() + '</div>'
     else:
         output += p.dataTable.renderToTable()
