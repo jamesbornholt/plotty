3b0559c053709f2d4ffc4c8c0e779b9280b3b567
James Bornholt <u4842199@anu.edu.au>
2014-08-12 16:00:59 +1000

    don't use the debug toolbar

--- settings.py
+++ settings.py
@@ -147,7 +147,7 @@ MIDDLEWARE_CLASSES = (
 #    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 #    'django.contrib.messages.middleware.MessageMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+#    'debug_toolbar.middleware.DebugToolbarMiddleware',
 #    'plotty.middleware.ProfileMiddleware'
 )
 
