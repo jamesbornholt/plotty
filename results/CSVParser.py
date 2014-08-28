8ac638963d0146504c4b5d11c2ddedd28a5fd805
James Bornholt <u4842199@anu.edu.au>
2014-08-28 22:49:44 +1000

    don't use DictReader and don't instantiate classes. ~2x speedup for csv parsing.

--- results/CSVParser.py
+++ results/CSVParser.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import csv
 import gzip
 import os
@@ -7,10 +8,7 @@ from Utilities import scenario_hash
 from Exceptions import PipelineError
 
 # a "Result" is a single iteration of a benchmark invocation
-class Result(object):
-    def __init__(self, scenario, value):
-        self.scenario = scenario
-        self.value = value
+Result = namedtuple('Result', ['scenario', 'value'])
 
 def parse_csv(csv_path):
     """ We support two different types of CSV. Each uses a column for each
@@ -26,21 +24,23 @@ def parse_csv(csv_path):
     """
 
     filename = str(os.path.basename(csv_path))
+
+    # We're going to open the file here just to figure out its headers
     if csv_path[-3:] == '.gz':
         f = gzip.open(csv_path, 'r')
     else:
         f = open(csv_path, 'rU')
     r = csv.DictReader(f)
-
-    # Work out which type we're working with
     headers = set(r.fieldnames)
+    f.seek(0) # let the workers decide how to work with the file
+
     if 'key' in headers and 'value' in headers:
         for h in headers:
             if h.startswith('value.'):
                 raise PipelineError("Couldn't detect the format of the CSV "
                     "file `%s`, because it has both 'key' and 'value' "
                     "headers and headers that start with 'value.'." % filename, "log file %s" % filename)
-        rows = _load_from_long_csv(r)
+        rows = _load_from_long_csv(f)
     else:
         for h in headers:
             if h.startswith('value.'):
@@ -51,22 +51,31 @@ def parse_csv(csv_path):
                 "or any columns that start with 'value.'. You "
                 "probably need to add 'value.' as a prefix to the file's "
                 "value variable columns, like 'value.bmtime'." % filename, "log file %s" % filename)
-        rows = _load_from_wide_csv(r)
+        rows = _load_from_wide_csv(f)
 
     f.close()
     return rows
 
-def _load_from_long_csv(reader):
-    headers = sorted(set(reader.fieldnames) - set(['key', 'value']))
+def _load_from_long_csv(f):
+    reader = csv.reader(f)
+
+    # the test to call _load_from_long_csv guarantees these values exist
+    headers = reader.next()
+    key_idx = headers.index("key")
+    headers.pop(key_idx)
+    val_idx = headers.index("value")
+    headers.pop(val_idx)
 
     scenarios = {}
     values = {}
     for row in reader:
-        key = row.pop('key')
-        val = row.pop('value')
-        sc = _scenario_hash_fast(row, headers)
+        if len(row) != len(headers)+2:
+            continue
+        key = row.pop(key_idx)
+        val = row.pop(val_idx)
+        sc = _scenario_hash_fast(row)
         if sc not in scenarios:
-            scenarios[sc] = row
+            scenarios[sc] = dict(zip(headers, row))
             values[sc] = []
         values[sc].append((key, val))
 
@@ -76,7 +85,9 @@ def _load_from_long_csv(reader):
         rows.append(row)
     return rows
 
-def _load_from_wide_csv(reader):
+def _load_from_wide_csv(f):
+    reader = csv.DictReader(f)
+
     value_columns = set([h for h in reader.fieldnames if h.startswith('value.')])
     scenario_columns = set(reader.fieldnames) - set(value_columns)
 
@@ -95,7 +106,5 @@ def _load_from_wide_csv(reader):
         rows.append(dr)
     return rows
 
-def _scenario_hash_fast(row, columns):
-    # csv parser guarantees that each x appears in row, so columns is used just
-    # for ordering
-    return tuple(row[x] for x in columns)
+def _scenario_hash_fast(row):
+    return hash(tuple(row))
